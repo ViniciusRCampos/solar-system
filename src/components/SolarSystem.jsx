061d3ff570d059e64f3ef6de13a1975f4e4ce6b8
ViniciusRCampos <vinicius_uo@hotmail.com>
2022-08-18 15:13:47 -0300

    Requisito 3

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Title from './Title';
 
 class SolarSystem extends React.Component {
   render() {
     return (
-      <div data-testid="solar-system" />
+      <div data-testid="solar-system">
+        <Title headline="Planetas" />
+      </div>
     );
   }
 }
